1941f0dbdfaf57d20cc72c527102ab98ef45782b
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-11-23 11:25:20 +0700

    feat: add form depends excel input

--- app.py
+++ app.py
@@ -2,8 +2,58 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+class App():
+
+    data = pd.DataFrame()
+    data_attributes = list()
+    data_form = dict()
+
+    def init_app(self):
+        st.title("Analisis")
+        st.sidebar.title("Input Your Data")
+
+    def get_data_excel(self):
+        uploaded_file = st.sidebar.file_uploader("Choose a excel file")
+        if uploaded_file is not None:
+
+            self.data = pd.read_excel(uploaded_file)
+            self.data_attributes = self.data.columns
+
+            self.show_data_dataframe()
+            self.show_form()
+
+    def show_data_dataframe(self):
+        col1, col2 = st.columns(2)
+        col1.text("Data :")
+        col1.write(self.data)
+
+        col2.text("Data Attributes :")
+        col2.write(self.data_attributes)
+
+    def show_form(self):
+
+        with st.sidebar.form("data_form"):
+
+            for attr in self.data_attributes:
+                if self.data[attr].dtype == 'int64' or self.data[attr].dtype == 'float64':
+                    self.data_form[attr] = st.number_input(attr)
+                else:
+                    self.data_form[attr] = st.selectbox(
+                        attr, self.data[attr].unique())
+
+            submitted = st.form_submit_button("Submit")
+
+            if (submitted):
+                self.show_data_input()
+
+    def show_data_input(self):
+
+        st.title("Data Input Value :")
+        for key, value in self.data_form.items():
+            st.write(key, ":", value)
+
+
+app = App()
+app.init_app()
+app.get_data_excel()
